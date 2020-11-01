52990a511383eda11f71101d2b05b24029665cc5
Amir Muhammad Hakim <amir.rhythm@gmail.com>
2020-11-01 22:55:53 +0700

    Vuetify grid system

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,42 @@
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
 
     <v-container class="my-16">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam libero,
-        eius ullam deleniti sit, dolores amet porro quo deserunt, recusandae
-        incidunt minima neque iusto animi architecto earum cupiditate ipsa
-        maxime.
-      </p>
+      <v-row no-gutters>
+        <v-col cols="12" md="6">
+          <v-btn outlined block color="primary">1</v-btn>
+        </v-col>
+        <v-col cols="4" md="2">
+          <v-btn outlined block color="primary">2</v-btn>
+        </v-col>
+        <v-col cols="4" md="2">
+          <v-btn outlined block color="primary">3</v-btn>
+        </v-col>
+        <v-col cols="4" md="2">
+          <v-btn outlined block color="primary">4</v-btn>
+        </v-col>
+      </v-row>
+
+      <v-spacer class="mt-8" />
+
+      <v-row no-gutters>
+        <v-col cols="4" md="3">
+          <v-btn outlined block color="error">1</v-btn>
+        </v-col>
+        <v-col cols="4" md="3">
+          <v-btn outlined block color="error">2</v-btn>
+        </v-col>
+      </v-row>
+
+      <v-spacer class="mt-8" />
+
+      <v-row no-gutters justify="end">
+        <v-col cols="4" md="3">
+          <v-btn outlined block color="success">1</v-btn>
+        </v-col>
+        <v-col cols="4" md="3">
+          <v-btn outlined block color="success">2</v-btn>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
