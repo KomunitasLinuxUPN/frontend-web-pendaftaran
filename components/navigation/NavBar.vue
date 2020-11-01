dd21466ad2b8830bc742c371d9f21315f8b3c41b
Amir Muhammad Hakim <amir.rhythm@gmail.com>
2020-11-02 03:29:22 +0700

    Avatars

--- components/navigation/NavBar.vue
+++ components/navigation/NavBar.vue
@@ -19,6 +19,15 @@
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawerIsOpen" app class="secondary">
+      <v-row justify="center" class="mt-5 text-center">
+        <v-col cols="12">
+          <v-avatar size="100">
+            <img src="/images/avatar-1.png" />
+          </v-avatar>
+          <p class="subtitle-1 white--text mt-3">Amir Muhammad Hakim</p>
+        </v-col>
+      </v-row>
+
       <v-list>
         <v-list-item v-for="link in links" :key="link.route" :to="link.route">
           <v-list-item-action>
@@ -38,10 +47,16 @@
 <script lang="ts">
 import { defineComponent, ref } from '@nuxtjs/composition-api'
 
+interface NavLink {
+  icon: string
+  text: string
+  route: string
+}
+
 export default defineComponent({
   setup() {
     const drawerIsOpen = ref(false)
-    const links = [
+    const links: NavLink[] = [
       { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
       { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
       { icon: 'mdi-account', text: 'Team', route: '/team' },
--- pages/team/index.vue
+++ pages/team/index.vue
@@ -13,7 +13,11 @@
           lg="3"
         >
           <v-card flat class="text-center">
-            <v-responsive class="pt-4">Image goes here</v-responsive>
+            <v-responsive class="pt-4">
+              <v-avatar size="80" class="grey lighten-2">
+                <img :src="person.avatar" />
+              </v-avatar>
+            </v-responsive>
             <v-card-text>
               <p class="subtitle-1 mb-2">{{ person.name }}</p>
               <p class="grey--text">{{ person.role }}</p>
@@ -39,6 +43,7 @@ interface Team {
   id: string
   name: string
   role: string
+  avatar: string
 }
 
 export default defineComponent({
@@ -47,11 +52,36 @@ export default defineComponent({
   },
   setup() {
     const team: Team[] = [
-      { id: uuid.v4(), name: 'Amir Hakim', role: 'Web developer' },
-      { id: uuid.v4(), name: 'Rafael', role: 'Graphic designer' },
-      { id: uuid.v4(), name: 'Doni Alfiando', role: 'Web developer' },
-      { id: uuid.v4(), name: 'Uzumaki Bayu', role: 'Social media maverick' },
-      { id: uuid.v4(), name: 'Uzumaki Saburo', role: 'Sales guru' },
+      {
+        id: uuid.v4(),
+        name: 'Amir Hakim',
+        role: 'Web developer',
+        avatar: 'images/avatar-1.png',
+      },
+      {
+        id: uuid.v4(),
+        name: 'Rafael',
+        role: 'Graphic designer',
+        avatar: 'images/avatar-2.png',
+      },
+      {
+        id: uuid.v4(),
+        name: 'Doni Alfiando',
+        role: 'Web developer',
+        avatar: 'images/avatar-3.png',
+      },
+      {
+        id: uuid.v4(),
+        name: 'Uzumaki Bayu',
+        role: 'Social media maverick',
+        avatar: 'images/avatar-4.png',
+      },
+      {
+        id: uuid.v4(),
+        name: 'Uzumaki Saburo',
+        role: 'Sales guru',
+        avatar: 'images/avatar-5.png',
+      },
     ]
 
     return {
